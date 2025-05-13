1f4d45193e79640cd162c683a8a2c3b3a4849d2b
Chen <160751168+Xcipio@users.noreply.github.com>
2025-05-14 03:29:49 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center px-4 py-10 text-gray-800">
-      <h1 className="text-4xl font-bold mb-2">Nap</h1>
+      <h1 className="text-4xl font-bold mb-2">5月14日</h1>
       <p className="text-lg text-center max-w-xl mb-6">
-        All We Need is PLAY!
+        早上好，小兔子！
       </p>
     </div>
   );
