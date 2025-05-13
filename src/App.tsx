3555caa7601bfc3d609171293af45394e4cdbb82
Chen <160751168+Xcipio@users.noreply.github.com>
2025-05-14 03:24:03 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center px-4 py-10 text-gray-800">
       <h1 className="text-4xl font-bold mb-2">Nap</h1>
       <p className="text-lg text-center max-w-xl mb-6">
-        Software Developer based in Tokyo. I love building elegant user experiences, solving complex problems, and drinking strong coffee.
+        All We Need is PLAY!
       </p>
     </div>
   );
