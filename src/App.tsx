f7d168cb29b009b15f038f4af08a64e23183ee3d
Chen <160751168+Xcipio@users.noreply.github.com>
2025-05-18 00:20:27 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 function App() {
-  const date = "20250514";
+  const date = "20250518";
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center px-4 py-10 text-gray-800">
       <h1 className="text-4xl font-bold mb-2">{date}</h1>
       <p className="text-lg text-center max-w-xl mb-6">
-        早上好，小兔子！
+        早上好，小兔子！你睡的怎么样呀
+        今天也很想你
       </p>
     </div>
   );
