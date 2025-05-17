dd8402ab4cb99cb85bfb9c954b3c09cdadebfb8e
Chen <160751168+Xcipio@users.noreply.github.com>
2025-05-18 00:23:59 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center px-4 py-10 text-gray-800">
       <h1 className="text-4xl font-bold mb-2">{date}</h1>
       <p className="text-lg text-center max-w-xl mb-6">
-        早上好，小兔子！<br>
-        你睡的怎么样呀？<br>
+        早上好，小兔子<br/>
+        你睡的怎么样呀？<br/>
         今天也很想你
       </p>
     </div>
